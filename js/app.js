3f88418559bed1b65daab380a7846db2779545b3
kmolumby <kelly.molumby@gmail.com>
2018-09-17 18:56:06 -0600

    data types

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// console.log('working'); 
+
+// 1. Data Types
+
+
+// A light switch that can be either on or off.
+
+// boolean - if (lightswitch = true) {
+//     return on
+// } else {
+//     return off
+// }
+
+// A user's email address.
+// string 
+// const emailAddress = "kmolumby@gmail.com"
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// array 
+
+// const spaceship = ('hull', 'laser blasters', 'tractor beam', 'warp drive')
+
+// A list of student names from our class.
+
+// array
+
+// const student = ('Kelly', 'Max', 'Phil')
+
+// A list of student names from our class, each with a location.
+
+// object 
+
+// const students = {
+//     kelly: {
+//         location: 'denver',
+//     }
+
+//     max: {
+//         location: 'denver', 
+//     }
+
+//     phil : {
+//         location: 'denver',
+//     }
+// }
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+// const students = {
+//     leslie: {
+//         location: 'denver',
+//         tvshows: ['friends', 'parks and rec']
+//     }
+
+//     ron: {
+//         location: 'denver', 
+//         tvshows: ['this old house', 'home improvement']
+//     }
+
+//     april : {
+//         location: 'denver',
+//         tvshows: ['the twighlight zone', ]
+//     }
+// }
