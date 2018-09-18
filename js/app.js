468689e77c122fa5a4c5bc0030b3272e9c2a5624
kmolumby <kelly.molumby@gmail.com>
2018-09-17 19:05:58 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -18,13 +18,13 @@
 // A spaceship with a hull, laser blasters, tractor beam, and warp drive.
 // array 
 
-// const spaceship = ('hull', 'laser blasters', 'tractor beam', 'warp drive')
+// const spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']
 
 // A list of student names from our class.
 
 // array
 
-// const student = ('Kelly', 'Max', 'Phil')
+// const student = ['Kelly', 'Max', 'Phil']
 
 // A list of student names from our class, each with a location.
 
@@ -61,3 +61,25 @@
 //         tvshows: ['the twighlight zone', ]
 //     }
 // }
+
+
+// 2. Take it Easy
+// Make an array that holds all of the colors of the rainbow.
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+// Write code that will access "blue" from the rainbow array.
+
+console.log(rainbow[4]);
+
+
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+const kelly = {
+    favoriteFood: 'lasagna',
+    hobby: 'photography',
+    currentTown: 'Denver',
+    favoriteDataType: 'object'
+}
+// Write code that will access your hobby from the object that you just created.
+console.log(kelly.hobby);
